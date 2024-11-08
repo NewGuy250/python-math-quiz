ef6f4b4dd90897120cbbff4d6b141cf98933686b
NewGuy250 <ndaniel220@yahoo.com>
2024-11-08 00:51:16 -0500

    added function to generate problem

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import random
+
+# Function for creating each problem
+def create_problem(operand, num1, num2):
+    while True:
+        try:
+            problem = f"{num1} {operand} {num2}"
+            answer = int(input(problem + "= "))
+            if answer == eval(problem):
+                return True
+            return False
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+
 def main():
-    pass
+    OPERANDS = ["+", "-", "*"]
+
+if __name__ == "__main__":
+    main()
